96d0e93430f50bd78227e0cfc554a017b5f98236
Cleiton <cleitonrmacedo@gmail.com>
2022-07-08 09:04:26 -0300

    feat: Adicionado lib HELMET para seguranca da aplicacao

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
+app.use(helmet());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
