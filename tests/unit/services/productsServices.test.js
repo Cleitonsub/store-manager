0b8dbfacf1baae02c44d056d1eb71ce576754762
Cleiton <cleitonrmacedo@gmail.com>
2022-07-10 12:29:41 -0300

    feat: Feito os testes de products.js do ./services

---
+++ tests/unit/services/productsServices.test.js
@@ -0,0 +1,33 @@
+const { expect } = require('chai');
+const productService = require('../../../services/products');
+
+const mockProducts = [
+  { id: 1, name: 'Martelo de Thor' },
+  { id: 2, name: 'Traje de encolhimento' },
+  { id: 3, name: 'Escudo do Capitão América' },
+];
+
+const mockProduct = { id: 1, name: 'Martelo de Thor' };
+
+describe('Teste ao chamar o service de products', () => {
+  describe('Quando é solicitado todos os produtos', () => {
+    it('retorna um array com todos os produtos', async () => {
+      const res = await productService.getAll();
+      // .deep para comparar objetos encontrado em pesquisa no stackoverflow, link:
+      // https://stackoverflow.com/questions/38497731/mocha-chai-uncaught-assertionerror-expected-to-equal-expected-actua
+      expect(res).to.be.deep.equal(mockProducts);
+    });
+  });
+
+  describe('Quando é solicitado um produto pelo id', () => {
+    it('é chamado com um id válido', async () => {
+      const res = await productService.getById(1);
+      expect(res).to.be.deep.equal(mockProduct);
+    });
+
+    it('é chamado com um id inválido', async () => {
+      const res = await productService.getById(4);
+      expect(res).to.be.null;
+    });
+  });
+});
